d941a29720ac0e367f1795fa3d79cad509646787
realfavey <oseniabiodunfavour@gmail.com>
2025-09-22 13:19:14 +0100

    Update settingss.js

--- settingss.js
+++ settingss.js
@@ -1,7 +1,7 @@
 
 module.exports = {
-  SESSION_ID: "",  // add your Session Id here
-  OWNER_NUMBER: "", // put your phone number here
+  SESSION_ID: "malvin~FiVz2Q7T#A3JfPiguLmxVmfF2htmvpU_I56Tn0Cy1r7yeLsbLbjc",  // add your Session Id here
+  OWNER_NUMBER: "2349122330415", // put your phone number here
   PREFIX: ".", // prefix (e.g., ., /, !, *)
   TIMEZONE: "Africa/Kampala" //put your country timeZone....leave blank if u don't know.
 };
